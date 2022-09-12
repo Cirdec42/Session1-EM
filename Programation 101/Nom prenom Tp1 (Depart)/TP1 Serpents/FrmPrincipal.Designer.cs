27e7b754fb566f4971cd839c09ea9198dfd08626
Cedric <cedricdaigneault2004@gmail.com>
2022-09-12 16:44:03 -0400

    TP1

---
+++ Programation 101/Nom prenom Tp1 (Depart)/TP1 Serpents/FrmPrincipal.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace TP2_Serpents
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.mnuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.mnuFichier = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFichierNouvellePartie = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFichierSeparateur = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFichierQuitter = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSpécial = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSpécialModeTest = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDéplacerJeton = new System.Windows.Forms.Button();
+            this.btnLancerDe = new System.Windows.Forms.Button();
+            this.imlDés = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.vsiDé = new VisualArrays.VisualCells.VisualInt();
+            this.sprFinPartie = new VisualArrays.TextSprite();
+            this.bvaGrille = new VisualArrays.BaseGrid();
+            this.sprJoueur = new VisualArrays.ImageSprite();
+            this.mnuPrincipal.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuPrincipal
+            // 
+            this.mnuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFichier,
+            this.mnuSpécial});
+            this.mnuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.mnuPrincipal.Name = "mnuPrincipal";
+            this.mnuPrincipal.Size = new System.Drawing.Size(792, 24);
+            this.mnuPrincipal.TabIndex = 0;
+            this.mnuPrincipal.Text = "menuStrip1";
+            // 
+            // mnuFichier
+            // 
+            this.mnuFichier.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFichierNouvellePartie,
+            this.mnuFichierSeparateur,
+            this.mnuFichierQuitter});
+            this.mnuFichier.Name = "mnuFichier";
+            this.mnuFichier.Size = new System.Drawing.Size(54, 20);
+            this.mnuFichier.Tag = "A14PC";
+            this.mnuFichier.Text = "Fichier";
+            // 
+            // mnuFichierNouvellePartie
+            // 
+            this.mnuFichierNouvellePartie.Name = "mnuFichierNouvellePartie";
+            this.mnuFichierNouvellePartie.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.mnuFichierNouvellePartie.Size = new System.Drawing.Size(197, 22);
+            this.mnuFichierNouvellePartie.Text = "Nouvelle Partie";
+            this.mnuFichierNouvellePartie.Click += new System.EventHandler(this.mnuFichierNouvellePartie_Click);
+            // 
+            // mnuFichierSeparateur
+            // 
+            this.mnuFichierSeparateur.Name = "mnuFichierSeparateur";
+            this.mnuFichierSeparateur.Size = new System.Drawing.Size(194, 6);
+            // 
+            // mnuFichierQuitter
+            // 
+            this.mnuFichierQuitter.Name = "mnuFichierQuitter";
+            this.mnuFichierQuitter.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.mnuFichierQuitter.Size = new System.Drawing.Size(197, 22);
+            this.mnuFichierQuitter.Text = "Quitter";
+            this.mnuFichierQuitter.Click += new System.EventHandler(this.mnuFichierQuitter_Click);
+            // 
+            // mnuSpécial
+            // 
+            this.mnuSpécial.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSpécialModeTest});
+            this.mnuSpécial.Name = "mnuSpécial";
+            this.mnuSpécial.Size = new System.Drawing.Size(56, 20);
+            this.mnuSpécial.Text = "Spécial";
+            // 
+            // mnuSpécialModeTest
+            // 
+            this.mnuSpécialModeTest.CheckOnClick = true;
+            this.mnuSpécialModeTest.Name = "mnuSpécialModeTest";
+            this.mnuSpécialModeTest.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.T)));
+            this.mnuSpécialModeTest.Size = new System.Drawing.Size(167, 22);
+            this.mnuSpécialModeTest.Text = "Mode test";
+            this.mnuSpécialModeTest.Click += new System.EventHandler(this.MnuSpécialModeTest_Click);
+            // 
+            // btnDéplacerJeton
+            // 
+            this.btnDéplacerJeton.Enabled = false;
+            this.btnDéplacerJeton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDéplacerJeton.Location = new System.Drawing.Point(13, 66);
+            this.btnDéplacerJeton.Name = "btnDéplacerJeton";
+            this.btnDéplacerJeton.Size = new System.Drawing.Size(152, 33);
+            this.btnDéplacerJeton.TabIndex = 15;
+            this.btnDéplacerJeton.Text = "&Déplacer le jeton";
+            this.btnDéplacerJeton.UseVisualStyleBackColor = true;
+            this.btnDéplacerJeton.Click += new System.EventHandler(this.btnDéplacerJeton_Click);
+            // 
+            // btnLancerDe
+            // 
+            this.btnLancerDe.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLancerDe.Location = new System.Drawing.Point(15, 27);
+            this.btnLancerDe.Name = "btnLancerDe";
+            this.btnLancerDe.Size = new System.Drawing.Size(150, 33);
+            this.btnLancerDe.TabIndex = 13;
+            this.btnLancerDe.Text = "&Lancer le dé";
+            this.btnLancerDe.UseVisualStyleBackColor = true;
+            this.btnLancerDe.Click += new System.EventHandler(this.btnLancerDé_Click);
+            // 
+            // imlDés
+            // 
+            this.imlDés.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imlDés.ImageStream")));
+            this.imlDés.TransparentColor = System.Drawing.Color.Transparent;
+            this.imlDés.Images.SetKeyName(0, "Vide.png");
+            this.imlDés.Images.SetKeyName(1, "1.png");
+            this.imlDés.Images.SetKeyName(2, "2.png");
+            this.imlDés.Images.SetKeyName(3, "3.png");
+            this.imlDés.Images.SetKeyName(4, "4.png");
+            this.imlDés.Images.SetKeyName(5, "5.png");
+            this.imlDés.Images.SetKeyName(6, "6.png");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLancerDe);
+            this.groupBox1.Controls.Add(this.vsiDé);
+            this.groupBox1.Controls.Add(this.btnDéplacerJeton);
+            this.groupBox1.Location = new System.Drawing.Point(513, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(259, 124);
+            this.groupBox1.TabIndex = 30;
+            this.groupBox1.TabStop = false;
+            // 
+            // vsiDé
+            // 
+            this.vsiDé.BackColor = System.Drawing.Color.White;
+            this.vsiDé.FocusColor = System.Drawing.Color.Gray;
+            this.vsiDé.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vsiDé.ImageList = this.imlDés;
+            this.vsiDé.Location = new System.Drawing.Point(173, 27);
+            this.vsiDé.Minimum = 0;
+            this.vsiDé.Name = "vsiDé";
+            this.vsiDé.ReadOnly = true;
+            this.vsiDé.Size = new System.Drawing.Size(71, 72);
+            this.vsiDé.TabIndex = 29;
+            this.vsiDé.Text = "visualInt1";
+            this.vsiDé.View = VisualArrays.enuIntView.ImageList;
+            // 
+            // sprFinPartie
+            // 
+            this.sprFinPartie.AlignOnGrid = true;
+            this.sprFinPartie.BackgroundShape = VisualArrays.enuShape.RoundRect;
+            this.sprFinPartie.DisplayIndex = -1;
+            this.sprFinPartie.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sprFinPartie.Location = new System.Drawing.Point(0, 220);
+            this.sprFinPartie.Name = "sprFinPartie";
+            this.sprFinPartie.Radius = 25;
+            this.sprFinPartie.ShapeColor = System.Drawing.Color.LightSteelBlue;
+            this.sprFinPartie.Size = new System.Drawing.Size(480, 40);
+            this.sprFinPartie.Text = "Fin de la partie";
+            this.sprFinPartie.TextColor = System.Drawing.Color.Olive;
+            this.sprFinPartie.Visible = false;
+            // 
+            // bvaGrille
+            // 
+            this.bvaGrille.AddressMode = VisualArrays.enuAddressMode.StairsBottomLeft;
+            this.bvaGrille.AddressView = VisualArrays.enuAddressView.Mode1D;
+            this.bvaGrille.BackgroundImage = global::SerpentsEchelles.Properties.Resources.Serpents;
+            this.bvaGrille.CellSize = new System.Drawing.Size(56, 56);
+            this.bvaGrille.ColumnCount = 8;
+            this.bvaGrille.EnabledAppearance.Style = VisualArrays.enuBkgStyle.None;
+            this.bvaGrille.GridAppearance.LineSize = 0;
+            this.bvaGrille.Location = new System.Drawing.Point(12, 45);
+            this.bvaGrille.Name = "bvaGrille";
+            this.bvaGrille.Padding = new System.Windows.Forms.Padding(0);
+            this.bvaGrille.RowCount = 8;
+            this.bvaGrille.RowHeader.ForeColor = System.Drawing.Color.White;
+            this.bvaGrille.Size = new System.Drawing.Size(480, 480);
+            this.bvaGrille.Sprites.AddRange(new VisualArrays.Sprite[] {
+            this.sprJoueur,
+            this.sprFinPartie});
+            this.bvaGrille.TabIndex = 24;
+            this.bvaGrille.TabStop = false;
+            this.bvaGrille.Click += new System.EventHandler(this.bvaGrille_Click);
+            // 
+            // sprJoueur
+            // 
+            this.sprJoueur.Animated = true;
+            this.sprJoueur.Duration = 250;
+            this.sprJoueur.FollowGrid = true;
+            this.sprJoueur.Image = global::SerpentsEchelles.Properties.Resources.cool_icon;
+            this.sprJoueur.Name = "sprJoueur";
+            this.sprJoueur.Size = new System.Drawing.Size(48, 48);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 537);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bvaGrille);
+            this.Controls.Add(this.mnuPrincipal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.mnuPrincipal;
+            this.MaximizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TP1 - Serpents et échelles -";
+            this.mnuPrincipal.ResumeLayout(false);
+            this.mnuPrincipal.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem mnuFichier;
+        private System.Windows.Forms.ToolStripMenuItem mnuFichierNouvellePartie;
+        private System.Windows.Forms.ToolStripMenuItem mnuFichierQuitter;
+        private System.Windows.Forms.Button btnDéplacerJeton;
+        private System.Windows.Forms.Button btnLancerDe;
+        private VisualArrays.BaseGrid bvaGrille;
+        private System.Windows.Forms.ToolStripSeparator mnuFichierSeparateur;
+        private VisualArrays.ImageSprite sprJoueur;
+        private VisualArrays.TextSprite sprFinPartie;
+        private VisualArrays.VisualCells.VisualInt vsiDé;
+        private System.Windows.Forms.ImageList imlDés;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolStripMenuItem mnuSpécial;
+        private System.Windows.Forms.ToolStripMenuItem mnuSpécialModeTest;
+    }
+}
+
---
+++ Programation 101/Nom prenom Tp1 (Depart)/TP1 Serpents/FrmPrincipal.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TP2_Serpents
+{
+    /// <summary>
+    /// Description : Jeu de serpents et échelles à un joueur.
+    /// 
+    /// Version     : 2020
+    /// </summary>
+    /// --------------------------------------------------------------------------------
+
+    public partial class FrmPrincipal : Form
+    {
+        #region NE PAS MODIFIER
+        public const string APP_INFO = " (Matériel)";
+        //------------------------------------------------------------------------------
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            btnLancerDe.Select();
+            this.Text += APP_INFO;
+            DémarrerNouvellePartie();
+        }
+
+        //----------------------------------------------------------------------------------
+        /// <summary>
+        /// Créer une nouvelle partie
+        /// </summary>
+        private void mnuFichierNouvellePartie_Click(object sender, EventArgs e)
+        {
+            DémarrerNouvellePartie();
+        }
+        //-----------------------------------------------------------------------------------
+        /// <summary>
+        /// Établir la valeur du déplacement
+        /// Mettre à jour les contrôles appropriés selon le contexte du jeu.
+        /// </summary>
+        private void btnLancerDé_Click(object sender, EventArgs e)
+        {
+            // Établir la valeur du déplacement
+            vsiDé.RandomizeValue(1, 6);
+
+            // Rendre disponible ou non les contrôles appropriés selon le contexte du jeu.
+            btnLancerDe.Enabled = false;
+            btnDéplacerJeton.Enabled = true;
+
+            // Activer le bon contrôle.
+            btnDéplacerJeton.Select();
+        }
+        #endregion
+
+        //------------------------------------------------------------------------------------
+        /// <summary>
+        /// Déplacer le jeton du joueur
+        /// Vérifier si fin de la partie
+        /// Mettre à jour les contrôles appropriés selon le contexte du jeu.
+        /// </summary>
+        private void btnDéplacerJeton_Click(object sender, EventArgs e)
+        {
+            //--------------------------------------------------------------------------------
+            // TODO 01A : Rendre non disponible le bouton pour déplacer le jeton
+            btnDéplacerJeton.Enabled = false;
+            // TODO 01B : Rendre disponible le bouton pour lancer le dé
+            btnLancerDe.Enabled = true;
+            // TODO 01C : Placer le focus sur le bouton pour lancer le dé
+            btnLancerDe.Focus();
+
+            // -------------------------------------------------------------------------------
+            // TODO 01D : Déplacer le jeton du joueur selon la valeur du dé
+            sprJoueur.DisplayIndex = sprJoueur.DisplayIndex + vsiDé.Value;
+            //      Déterminer la position future et si elle est jouable.
+            //      Déplacer le jeton à cette position
+            //      Déterminer si un deuxième déplacement est nécessaire et l'effectuer
+           
+            switch (sprJoueur.DisplayIndex)
+            {
+                //Case Serpent
+                case 18:
+                    sprJoueur.FollowGrid = false;
+                    sprJoueur.DisplayIndex = 4;
+                    sprJoueur.FollowGrid = true;
+                    break;
+                case 26:
+                    sprJoueur.FollowGrid = false;
+                    sprJoueur.DisplayIndex = 7;
+                    sprJoueur.FollowGrid = true;
+                    break;
+                case 57:
+                    sprJoueur.FollowGrid = false;
+                    sprJoueur.DisplayIndex = 40;
+                    sprJoueur.FollowGrid = true;
+                    break;
+                case 61:
+                    sprJoueur.FollowGrid = false;
+                    sprJoueur.DisplayIndex = 31;
+                    sprJoueur.FollowGrid= true;
+                    break;
+
+                //Case echelle
+                case 2:
+                    sprJoueur.FollowGrid = false;
+                    sprJoueur.DisplayIndex = 17;
+                    sprJoueur.FollowGrid = true;
+                    break;
+                case 28:
+                    sprJoueur.FollowGrid = false;
+                    sprJoueur.DisplayIndex = 52;
+                    sprJoueur.FollowGrid = true;
+                    break;
+                case 23:
+                    sprJoueur.FollowGrid = false;
+                    sprJoueur.DisplayIndex = 38;
+                    sprJoueur.FollowGrid = true;
+                    break;
+                case 47:
+                    sprJoueur.FollowGrid = false;
+                    sprJoueur.DisplayIndex = 61;
+                    sprJoueur.FollowGrid = true;
+                    break;
+
+                //Soleil
+                case 15:
+                case 58:
+                    sprJoueur.Animated = false;
+                    sprJoueur.DisplayIndex = 63;
+                    sprJoueur.Animated = true;
+                    break;
+
+                //Serpents enroullés
+                case 42:
+                case 51:
+                    sprJoueur.Animated = false;
+                    sprJoueur.DisplayIndex = 0;
+                    sprJoueur.Animated = true;
+                    break;
+
+                    
+                default:
+                    break;
+            }
+
+
+            // -------------------------------------------------------------------------------
+            // TODO 01E : Arréter la partie
+            //      Vérifier si on atteint le sommet
+            //      Si le sommet est atteint, mettre à jour les contrôles pour arrêter la partie 
+            if (sprJoueur.DisplayIndex == 63)
+            {
+                sprFinPartie.Visible = true;
+                btnDéplacerJeton.Enabled = false;
+                btnLancerDe.Enabled = false;
+            }
+
+        }
+        //----------------------------------------------------------------------------------
+        /// <summary>
+        /// Préparer le jeu pour une nouvelle partie
+        /// Mettre à jour les contrôles appropriés selon le contexte du jeu.
+        /// </summary>
+        private void DémarrerNouvellePartie()
+        {
+            // TODO 02A : Préparer l'état du formulaire pour une nouvelle partie.
+            sprJoueur.Animated = false;
+            sprJoueur.DisplayIndex = 0;
+            sprJoueur.Animated = true;
+            sprJoueur.FollowGrid = true;
+            btnLancerDe.Enabled = true;
+
+
+
+            // TODO 02B : Rendre disponible ou non les contrôles appropriés selon le contexte du jeu.
+
+
+
+            #region NE PAS MODIFIER
+            btnLancerDe.Select();
+            mnuSpécialModeTest.Checked = false;
+            vsiDé.ReadOnly = true;
+            #endregion
+        }
+
+        #region NE PAS MODIFIER
+        //-----------------------------------------------------------------------------------------------
+        private void MnuSpécialModeTest_Click(object sender, EventArgs e)
+        {
+            vsiDé.ReadOnly = !mnuSpécialModeTest.Checked || sprFinPartie.Visible;
+        }
+        //-----------------------------------------------------------------------------------------------
+        private void mnuFichierQuitter_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        #endregion
+
+        private void bvaGrille_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
