b09e4daf01378cfbe81c1bcd192003515982e7e9
Cedric <cedricdaigneault2004@gmail.com>
2022-10-17 08:35:25 -0400

    Labo 3 Prog

---
+++ Programation 101/R14 Laboratoire #3 (Matériel) v22.1.1/R14 Laboratoire #3 (Matériel) v22.1.1/Nom prénom Labo3 (Départ)/AppSudoku/FrmPrincipal.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace AppSudoku
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.mnuFichier = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFichierNouvellePartie = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFichierSep1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFichierQuitter = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAide = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAideSolutionner = new System.Windows.Forms.ToolStripMenuItem();
+            this.panSeparateurVertical1 = new System.Windows.Forms.Panel();
+            this.panSeparateurVertical2 = new System.Windows.Forms.Panel();
+            this.panSeparateurHorizontal1 = new System.Windows.Forms.Panel();
+            this.panSeparateurHorizontal2 = new System.Windows.Forms.Panel();
+            this.panJeu = new System.Windows.Forms.Panel();
+            this.viaGrille = new VisualArrays.VisualIntArray();
+            this.menuPrincipal.SuspendLayout();
+            this.panJeu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuPrincipal
+            // 
+            this.menuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFichier,
+            this.mnuAide});
+            this.menuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuPrincipal.Name = "menuPrincipal";
+            this.menuPrincipal.Size = new System.Drawing.Size(462, 24);
+            this.menuPrincipal.TabIndex = 6;
+            this.menuPrincipal.Text = "menuStrip1";
+            // 
+            // mnuFichier
+            // 
+            this.mnuFichier.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFichierNouvellePartie,
+            this.mnuFichierSep1,
+            this.mnuFichierQuitter});
+            this.mnuFichier.Name = "mnuFichier";
+            this.mnuFichier.Size = new System.Drawing.Size(54, 20);
+            this.mnuFichier.Text = "&Fichier";
+            // 
+            // mnuFichierNouvellePartie
+            // 
+            this.mnuFichierNouvellePartie.Name = "mnuFichierNouvellePartie";
+            this.mnuFichierNouvellePartie.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.mnuFichierNouvellePartie.Size = new System.Drawing.Size(197, 22);
+            this.mnuFichierNouvellePartie.Text = "&Nouvelle partie";
+            this.mnuFichierNouvellePartie.Click += new System.EventHandler(this.mnuFichierNouvellePartie_Click);
+            // 
+            // mnuFichierSep1
+            // 
+            this.mnuFichierSep1.Name = "mnuFichierSep1";
+            this.mnuFichierSep1.Size = new System.Drawing.Size(194, 6);
+            // 
+            // mnuFichierQuitter
+            // 
+            this.mnuFichierQuitter.Name = "mnuFichierQuitter";
+            this.mnuFichierQuitter.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.mnuFichierQuitter.Size = new System.Drawing.Size(197, 22);
+            this.mnuFichierQuitter.Text = "&Quitter";
+            this.mnuFichierQuitter.Click += new System.EventHandler(this.mnuFichierQuitter_Click);
+            // 
+            // mnuAide
+            // 
+            this.mnuAide.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAideSolutionner});
+            this.mnuAide.Name = "mnuAide";
+            this.mnuAide.Size = new System.Drawing.Size(43, 20);
+            this.mnuAide.Text = "&Aide";
+            // 
+            // mnuAideSolutionner
+            // 
+            this.mnuAideSolutionner.Name = "mnuAideSolutionner";
+            this.mnuAideSolutionner.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.mnuAideSolutionner.Size = new System.Drawing.Size(175, 22);
+            this.mnuAideSolutionner.Text = "&Solutionner";
+            this.mnuAideSolutionner.Click += new System.EventHandler(this.mnuAideSolutionner_Click);
+            // 
+            // panSeparateurVertical1
+            // 
+            this.panSeparateurVertical1.BackColor = System.Drawing.Color.Gray;
+            this.panSeparateurVertical1.Location = new System.Drawing.Point(146, 13);
+            this.panSeparateurVertical1.Name = "panSeparateurVertical1";
+            this.panSeparateurVertical1.Size = new System.Drawing.Size(5, 370);
+            this.panSeparateurVertical1.TabIndex = 7;
+            // 
+            // panSeparateurVertical2
+            // 
+            this.panSeparateurVertical2.BackColor = System.Drawing.Color.Gray;
+            this.panSeparateurVertical2.Location = new System.Drawing.Point(279, 13);
+            this.panSeparateurVertical2.Name = "panSeparateurVertical2";
+            this.panSeparateurVertical2.Size = new System.Drawing.Size(5, 370);
+            this.panSeparateurVertical2.TabIndex = 8;
+            // 
+            // panSeparateurHorizontal1
+            // 
+            this.panSeparateurHorizontal1.BackColor = System.Drawing.Color.Gray;
+            this.panSeparateurHorizontal1.Location = new System.Drawing.Point(13, 134);
+            this.panSeparateurHorizontal1.Name = "panSeparateurHorizontal1";
+            this.panSeparateurHorizontal1.Size = new System.Drawing.Size(406, 5);
+            this.panSeparateurHorizontal1.TabIndex = 8;
+            // 
+            // panSeparateurHorizontal2
+            // 
+            this.panSeparateurHorizontal2.BackColor = System.Drawing.Color.Gray;
+            this.panSeparateurHorizontal2.Location = new System.Drawing.Point(13, 257);
+            this.panSeparateurHorizontal2.Name = "panSeparateurHorizontal2";
+            this.panSeparateurHorizontal2.Size = new System.Drawing.Size(406, 5);
+            this.panSeparateurHorizontal2.TabIndex = 9;
+            // 
+            // panJeu
+            // 
+            this.panJeu.BackColor = System.Drawing.Color.Green;
+            this.panJeu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panJeu.Controls.Add(this.panSeparateurHorizontal2);
+            this.panJeu.Controls.Add(this.panSeparateurVertical2);
+            this.panJeu.Controls.Add(this.panSeparateurHorizontal1);
+            this.panJeu.Controls.Add(this.panSeparateurVertical1);
+            this.panJeu.Controls.Add(this.viaGrille);
+            this.panJeu.Location = new System.Drawing.Point(15, 40);
+            this.panJeu.Name = "panJeu";
+            this.panJeu.Size = new System.Drawing.Size(433, 398);
+            this.panJeu.TabIndex = 11;
+            // 
+            // viaGrille
+            // 
+            this.viaGrille.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.viaGrille.CellMargin = 4;
+            this.viaGrille.CellSize = new System.Drawing.Size(36, 32);
+            this.viaGrille.ColumnCount = 9;
+            this.viaGrille.DisabledAppearance.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.viaGrille.DisabledAppearance.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viaGrille.DisabledAppearance.Shape = VisualArrays.enuShape.RoundRect;
+            this.viaGrille.DisabledAppearance.Style = VisualArrays.enuBkgStyle.FillShape;
+            this.viaGrille.DisabledAppearance.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.viaGrille.EnabledAppearance.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.viaGrille.EnabledAppearance.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viaGrille.EnabledAppearance.Shape = VisualArrays.enuShape.RoundRect;
+            this.viaGrille.EnabledAppearance.TextColor = System.Drawing.Color.Black;
+            this.viaGrille.GridAppearance.Color = System.Drawing.Color.Silver;
+            this.viaGrille.Location = new System.Drawing.Point(6, 7);
+            this.viaGrille.Maximum = 9;
+            this.viaGrille.Minimum = 0;
+            this.viaGrille.Name = "viaGrille";
+            this.viaGrille.RowCount = 9;
+            this.viaGrille.RowHeader.ForeColor = System.Drawing.Color.White;
+            this.viaGrille.SelectionAppearance.Padding = new System.Windows.Forms.Padding(2, 2, 1, 1);
+            this.viaGrille.SelectionAppearance.Shape = VisualArrays.enuShape.RoundRect;
+            this.viaGrille.SelectionMode = System.Windows.Forms.SelectionMode.One;
+            this.viaGrille.Size = new System.Drawing.Size(418, 382);
+            this.viaGrille.SpecialValue = 0;
+            this.viaGrille.TabIndex = 12;
+            this.viaGrille.ValueChanged += new System.EventHandler<VisualArrays.ValueChangedEventArgs>(this.viaGrille_ValueChanged);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 453);
+            this.Controls.Add(this.menuPrincipal);
+            this.Controls.Add(this.panJeu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jeu du Sudoku ";
+            this.menuPrincipal.ResumeLayout(false);
+            this.menuPrincipal.PerformLayout();
+            this.panJeu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem mnuFichier;
+        private System.Windows.Forms.ToolStripMenuItem mnuFichierQuitter;
+        private System.Windows.Forms.Panel panSeparateurVertical1;
+        private System.Windows.Forms.Panel panSeparateurVertical2;
+        private System.Windows.Forms.Panel panSeparateurHorizontal1;
+        private System.Windows.Forms.Panel panSeparateurHorizontal2;
+        private System.Windows.Forms.ToolStripMenuItem mnuFichierNouvellePartie;
+        private System.Windows.Forms.ToolStripSeparator mnuFichierSep1;
+        private System.Windows.Forms.Panel panJeu;
+        private VisualArrays.VisualIntArray viaGrille;
+        private System.Windows.Forms.ToolStripMenuItem mnuAide;
+        private System.Windows.Forms.ToolStripMenuItem mnuAideSolutionner;
+    }
+}
+
---
+++ Programation 101/R14 Laboratoire #3 (Matériel) v22.1.1/R14 Laboratoire #3 (Matériel) v22.1.1/Nom prénom Labo3 (Départ)/AppSudoku/FrmPrincipal.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using VisualArrays;
+
+namespace AppSudoku
+{
+    public partial class FrmPrincipal : Form
+    {
+        /// <summary>
+        /// NOTIONS :
+        ///  - Copier des éléments d'un tableau 2D dans une grille
+        ///  - Désactiver des cellules de la grille
+        ///  - Utiliser une valeur spéciale dont l'apparence est différente des autres valeurs
+        ///  - Parcourir les cellules d'une grille en deux dimensions
+        ///     - Parcourir des rangées, des colonnes ou des zones de 3 X 3
+        ///  - Utiliser un tableau de booléens pour marquer les nombres utilisés.
+        /// </summary>
+
+        public const string APP_INFO = "(Matériel)";
+
+        #region Sudoku Moyen (SEUL LE MOYEN EST UTILISÉ)
+        int[,] m_tabSudokuMoyen =
+        {
+            {-5,2,4,3,1,7,-8,6,-9},
+            {3,1,7,-6,-8,-9,4,5,2},
+            {-8,-9,6,4,-2,5,1,-3,7},
+            {2,8,1,-9,3,-6,5,-7,4},
+            {-4,6,-5,8,-7,2,-3,9,-1},
+            {9,-7,3,-5,4,-1,6,2,8},
+            {7,-5,8,1,-9,3,2,-4,-6},
+            {6,4,9,-2,-5,-8,7,1,3},
+            {-1,3,-2,7,6,4,9,8,-5}
+        };
+        #region Sudoku Facile
+        int[,] m_tabSudokuFacile = {
+            {5,1,-2,-4,8,6,9,-7,-3},
+            {6,-7,-9,2,3,-1,-5,-4,8},
+            {-8,4,3,-5,9,-7,2,6,-1},
+            {2,3,5,7,1,8,4,9,-6},
+            {7,-9,1,-3,-6,-4,8,-2,5},
+            {-4,6,8,9,5,2,3,1,7},
+            {-1,5,7,-8,2,-9,6,3,-4},
+            {3,-2,-6,-1,4,5,-7,8,9},
+            {-9,-8,4,6,7,-3,-1,5,2},
+        };
+        #endregion
+        #region Sudoku Difficile
+        int[,] m_tabSudokuDifficile =
+        {
+            {5,9,6,-2,7,-4,-8,1,3},
+            {-7,2,8,1,3,9,6,-4,-5},
+            {4,-3,1,5,8,-6,-2,7,-9},
+            {-9,4,3,-6,-1,2,-5,8,7},
+            {-8,5,2,3,9,7,1,6,-4},
+            {6,1,-7,4,-5,-8,9,3,-2},
+            {-3,8,-4,-9,6,5,7,-2,1},
+            {-1,-7,5,8,2,3,4,9,-6},
+            {2,6,-9,-7,4,-1,3,5,8 }
+        };
+        #endregion
+        #endregion
+
+        #region NE PAS MODIFIER
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            Text += APP_INFO;
+            PréparerNouvellePartie();
+        }
+        /// <summary>
+        /// Démarre une nouvelle partie
+        /// </summary>
+        private void mnuFichierNouvellePartie_Click(object sender, EventArgs e)
+        {
+            PréparerNouvellePartie();
+        }
+        //================================================================================================
+        private void mnuFichierQuitter_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        #endregion
+
+        #region PréparerNouvellePartie
+        //================================================================================================
+        /// <summary>
+        /// TODO 01 : Copier uniquement les valeurs négatives (à rendre positive) du tableau m_tabSudokuMoyen 
+        /// dans la grille et désélectionner les cellules recevant les valeurs, il s'agit des valeurs fixes 
+        /// du jeu Sudoku. Les autres cellules vont contenir la valeur spéciale 0.
+        /// </summary>
+        private void PréparerNouvellePartie()
+        {
+			// À COMPLÉTER...
+
+        }
+        #endregion
+
+        #region Solutionner le Sudoku
+        //================================================================================================
+        /// <summary>
+        /// TODO 02 : Afficher la solution du Sudoku en copiant toutes les valeurs du tableau m_tabSudokuMoyen
+        /// dans la grille. Attention : les valeurs fixes du jeu sont négatives et les autres sont positives.
+        /// Désactiver l'option pour solutionner, et mettre le SelectedIndex à -1 pour la grille.
+        /// </summary>
+        private void mnuAideSolutionner_Click(object sender, EventArgs e)
+        {
+			// À COMPLÉTER...
+
+        }
+        #endregion
+
+        #region VérifierLaFinDeLaPartie
+        /// <summary>
+        /// TODO 03 : Vérifier la fin d'une partie, vous devez compter le nombre de valeurs égale à 0,
+        /// S'il n'y a aucune valeur à 0 et que : panJeu.BackColor == Color.Green alors la partie est terminée
+        /// Il faut rendre inactive la grille et changer la couleur du panJeu pour Color.Orange
+        /// 
+        /// </summary>
+        private void VérifierLaFinDeLaPartie()
+        {
+			// À COMPLÉTER...
+
+        }
+        #endregion
+
+        #region Vérifier l'intégrité du Sudoku
+        //================================================================================================
+        /// <summary>
+        /// TODO 04* : En utilisant un tableau de booléens, vous devez parcourir toutes les rangées afin 
+        /// de mettre en rouge avec : panJeu.BackColor = Color.Red; le panneau panJeu si deux valeurs autres
+        /// que la valeur spéciale se retrouvent sur une même rangée. 
+        /// 
+        /// Voici le tableau de booléens à utiliser :
+        /// bool[] tabDesChiffresPrésents = { false, false, false, false, false, false, false, false, false, false };
+        /// </summary>
+        private void VérifierToutesLesRangées()
+        {
+			// À COMPLÉTER...
+
+        }
+        //================================================================================================
+        /// <summary>
+        /// TODO 05* : En utilisant un tableau de booléens, vous devez parcourir toutes les colonnes afin 
+        /// de mettre en rouge avec : panJeu.BackColor = Color.Red; le panneau panJeu si deux valeurs autres
+        /// que la valeur spéciale se retrouvent sur une même colonne. 
+        /// 
+        /// Voici le tableau de booléens à utiliser :
+        /// bool[] tabDesChiffresPrésents = { false, false, false, false, false, false, false, false, false, false };
+        /// </summary>
+        private void VérifierToutesLesColonnes()
+        {
+			// À COMPLÉTER...
+
+        }
+        //================================================================================================
+        /// <summary>
+        /// TODO 06*** : En utilisant un tableau de booléens, vous devez parcourir toutes les regions afin 
+        /// de mettre en rouge avec : panJeu.BackColor = Color.Red; le panneau panJeu si deux valeurs autres
+        /// que la valeur spéciale se retrouvent sur une même région. 
+        /// 
+        /// Voici le tableau de booléens à utiliser :
+        /// bool[] tabDesChiffresPrésents = { false, false, false, false, false, false, false, false, false, false };
+        /// </summary>
+        private void VérifierToutesLesRegions()
+        {
+			// À COMPLÉTER...
+
+        }
+        #endregion
+
+        #region viaGrille_ValueChanged (À CONSULTER/NE PAS MODIFIER)
+        //================================================================================================
+        /// <summary>
+        /// Vérifie l'intégrité du jeu et si la partie est terminée à chaque changement de valeur dans la grille.
+        /// </summary>
+        private void viaGrille_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            panJeu.BackColor = Color.Green;
+            VérifierToutesLesColonnes();
+            VérifierToutesLesRangées();
+            VérifierToutesLesRegions();
+            VérifierLaFinDeLaPartie();
+        }
+        #endregion
+    }
+}
